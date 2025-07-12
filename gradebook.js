0661e16313eefd934a904236fad716abc6d36b4b
hasanALQ <halquzweeni@gmail.com>
2025-07-12 10:08:37 -0400

    Update gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -1,16 +1,10 @@
-// TODO: Fetch data from the PostgreSQL database (to be implemented later)
 function fetchGradeData() {
-  // This function will query the PostgreSQL database and return grade data
   console.log("Fetching grade data...");
 }
 
-// TODO: Populate the table with grade data
 function populateGradebook(data) {
-  // This function will take the fetched grade data and populate the table
   console.log("Populating gradebook with data:", data);
 }
 
-// TODO: REMOVE THIS
-// Call these stubs to demonstrate the workflow
 const gradeData = fetchGradeData();
 populateGradebook(gradeData);
